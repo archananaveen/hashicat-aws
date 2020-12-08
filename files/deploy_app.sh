2f66eda2527e62857c109edc26bebabf30d23dd6
Archana Naveen <archana.x.naveen@us.hsbc.com>
2020-12-08 17:14:51 +0000

    test

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own.
+  Welcome to ${PREFIX}'s app. Replace this 333333333333333$$$$$$$$$$$$4444text with your own.
   <!-- END -->
 
   </div>
